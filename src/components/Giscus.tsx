56aedf2e188b4aa24a4ff2c75e857e6d6fc3953b
CoodingPenguin <coodingpenguin@gmail.com>
2023-02-25 22:25:09 +0900

    Add giscus comment component in essay

---
+++ src/components/Giscus.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Giscus from '@giscus/react';
+import {useColorMode} from '@docusaurus/theme-common';
+
+const GiscusComment = () => {
+  const {colorMode} = useColorMode();
+  const giscusTheme = colorMode === 'light' ? 'light' : 'dark';
+  return (
+    <div style={{padding: '1rem'}}>
+      <Giscus
+        id='comments'
+        repo='CoodingPenguin/devlog'
+        repoId='R_kgDOI3OU9Q'
+        category='댓글'
+        categoryId='DIC_kwDOI3OU9c4CUdU0'
+        mapping='pathname'
+        reactionsEnabled='0'
+        emitMetadata='0'
+        inputPosition='bottom'
+        strict='0'
+        lang='ko'
+        theme={giscusTheme}
+      />
+    </div>
+  )
+};
+
+export default GiscusComment;
