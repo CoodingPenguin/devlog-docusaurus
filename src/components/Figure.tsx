24dadf641f0824b3a33e39a72b1d46dc18d62b79
CoodingPenguin <coodingpenguin@gmail.com>
2023-02-25 21:17:31 +0900

    Add border option to "Figure"

--- src/components/Figure.tsx
+++ src/components/Figure.tsx
@@ -4,19 +4,21 @@ import isUrl from '@site/src/utils/isUrl';
 
 type FigureProps = {
   src: string;
-  caption: string;
+  caption?: string;
+  captionLink?: string;
   alt?: string;
+  hasBorder?: boolean;
 }
 
-const Figure = ({src, caption, alt}: FigureProps) => (
+const Figure = ({src, caption, captionLink, alt, hasBorder = false}: FigureProps) => (
   <figure style={{textAlign: 'center', marginLeft: 0, marginRight: 0}}>
-    <img src={useBaseUrl(src)} alt={alt || caption} style={{borderRadius: '8px'}} width='100%'/>
+    <img src={useBaseUrl(src)} alt={alt || caption} style={{borderRadius: '8px', border: hasBorder ? '1px solid #dfdfdf' : 'none'}} width='100%'/>
     <figcaption style={{fontSize: '85%', fontStyle: 'italic', opacity: '70%', fontWeight: '600'}}>
       {
-        isUrl(caption) ? (
+        captionLink && isUrl(captionLink) ? (
           <div>
             <span>출처: </span>
-            <a href={caption}>{caption}</a>
+            <a href={captionLink}>{caption || captionLink}</a>
           </div>
         ) : caption
       }
