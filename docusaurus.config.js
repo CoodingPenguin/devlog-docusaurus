3174c3d8f11cf20373b60b60ef3779dc4ae8e12c
CoodingPenguin <coodingpenguin@gmail.com>
2023-05-06 16:27:08 +0900

    :wrench: Create oss docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,104 +1,94 @@
 // @ts-nocheck
-// Note: type annotations allow type checking and IDEs autocompletion
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const remarkMath = require('remark-math');
-const rehypeKatex = require('rehype-katex');
-const pluginImageZoom = require('docusaurus-plugin-image-zoom');
-
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const remarkMath = require("remark-math");
+const rehypeKatex = require("rehype-katex");
+const pluginImageZoom = require("docusaurus-plugin-image-zoom");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: '코딩하는펭귄의 저장소',
-  tagline: '코딩하는펭귄의 개발 이모저모',
-  favicon: '/favicon.ico',
-  titleDelimiter: '@',
-
-  // Set the production url of your site here
-  url: 'https://coodingpenguin.xyz',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // Set trailing slash option.
-  // ref: https://melonicedlatte.com/2023/02/05/232900.html
-  // trailingSlash: false,
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'coodingpenguin', // Usually your GitHub org/user name.
-  projectName: 'devlog', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
+  title: "코딩하는펭귄의 저장소",
+  tagline: "코딩하는펭귄의 개발 이모저모",
+  favicon: "/favicon.ico",
+  titleDelimiter: "@",
+  url: "https://coodingpenguin.xyz",
+  baseUrl: "/",
+  organizationName: "coodingpenguin", // Usually your GitHub org/user name.
+  projectName: "devlog", // Usually your repo name.
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
   i18n: {
-    defaultLocale: 'ko',
-    locales: ['ko'],
+    defaultLocale: "ko",
+    locales: ["ko"],
   },
 
   plugins: [
     pluginImageZoom,
+    [
+      "@docusaurus/plugin-content-docs",
+      {
+        id: "oss",
+        path: "oss",
+        routeBasePath: "/oss",
+        sidebarPath: require.resolve("./sidebars/sidebarsOss.js"),
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+      },
+    ],
   ],
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          path: 'wiki',
-          routeBasePath: '/wiki',
-          sidebarPath: require.resolve('./sidebars.js'),
+          path: "wiki",
+          routeBasePath: "/wiki",
+          sidebarPath: require.resolve("./sidebars/sidebarsWiki.js"),
           remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
         },
         blog: {
-          path: 'essay',
-          routeBasePath: '/essay',
-          blogSidebarCount: 'ALL',
-          blogSidebarTitle: '끄적이는 중',
+          path: "essay",
+          routeBasePath: "/essay",
+          blogSidebarCount: "ALL",
+          blogSidebarTitle: "끄적이는 중",
           postsPerPage: 3,
           showReadingTime: false,
           feedOptions: {
-            type: 'all',
-            title: '코딩하는펭귄의 끄적끄적',
-            description: '코딩하는펭귄은 글쓰는 중',
+            type: "all",
+            title: "코딩하는펭귄의 끄적끄적",
+            description: "코딩하는펭귄은 글쓰는 중",
             copyright: `Copyright © ${new Date().getFullYear()} 코딩하는펭귄(CoodingPenguin)`,
-            language: 'ko',
+            language: "ko",
           },
           remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
         },
         theme: {
-          customCss: require.resolve('./src/css/global.css'),
+          customCss: require.resolve("./src/css/global.css"),
         },
         sitemap: {
-          changefreq: 'daily',
+          changefreq: "daily",
           priority: 0.5,
-          ignorePatterns: [
-            '/essay/tags/**',
-            '/wiki/tags/**'
-          ],
-          filename: 'sitemap.xml',
+          ignorePatterns: ["/essay/tags/**", "/wiki/tags/**"],
+          filename: "sitemap.xml",
         },
         gtag: {
-          trackingID: 'G-53DQ06C3MB'
-        }
+          trackingID: "G-53DQ06C3MB",
+        },
       }),
     ],
   ],
 
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
 
@@ -106,46 +96,51 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       algolia: {
-        appId: '6GO6RGX99R',
-        apiKey: '54416d3dc7a6a8a8f32b019a8b562fd7',
-        indexName: 'coodingpenguin',
+        appId: "6GO6RGX99R",
+        apiKey: "54416d3dc7a6a8a8f32b019a8b562fd7",
+        indexName: "coodingpenguin",
         contextualSearch: true,
         searchParameters: {},
-        searchPagePath: 'search',
+        searchPagePath: "search",
         debug: false,
       },
-      image: '/social-card.png',
+      image: "/social-card.png",
       navbar: {
-        title: '코딩하는펭귄의 저장소',
+        title: "코딩하는펭귄의 저장소",
         logo: {
-          alt: 'CoodingPenguin Devlog Logo',
-          src: '/logo.png',
+          alt: "CoodingPenguin Devlog Logo",
+          src: "/logo.png",
         },
         items: [
           {
-            to: '/wiki',
-            label: '개발위키',
-            position: 'left',
+            to: "/wiki",
+            label: "개발위키",
+            position: "left",
           },
           {
-            to: '/essay',
-            label: '끄적끄적',
-            position: 'left'
+            to: "/oss",
+            label: "오픈소스로 배우기",
+            position: "left",
           },
           {
-            href: 'https://github.com/coodingpenguin',
-            position: 'right',
-            className: 'header-link header-github-link'
+            to: "/essay",
+            label: "끄적끄적",
+            position: "left",
           },
           {
-            href: 'https://www.instagram.com/cooding_penguin',
-            position: 'right',
-            className: 'header-link header-instagram-link'
-          }
+            href: "https://github.com/coodingpenguin",
+            position: "right",
+            className: "header-link header-github-link",
+          },
+          {
+            href: "https://www.instagram.com/cooding_penguin",
+            position: "right",
+            className: "header-link header-instagram-link",
+          },
         ],
       },
       footer: {
-        style: 'light',
+        style: "light",
         copyright: `Copyright © ${new Date().getFullYear()} 코딩하는펭귄(CoodingPenguin)`,
       },
       prism: {
@@ -153,15 +148,14 @@ const config = {
         darkTheme: darkCodeTheme,
       },
       zoom: {
-        selector: '.markdown :not(em) > img',
+        selector: ".markdown :not(em) > img",
         config: {
-          // options you can specify via https://github.com/francoischalifour/medium-zoom#usage
           background: {
-            light: 'rgb(255, 255, 255)',
-            dark: 'rgb(50, 50, 50)'
-          }
-        }
-      }
+            light: "rgb(255, 255, 255)",
+            dark: "rgb(50, 50, 50)",
+          },
+        },
+      },
     }),
 };
 
