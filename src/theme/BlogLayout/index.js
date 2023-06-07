be1d46a7fe478b4ccf4f0e4a17debd2da6c1b9ba
CoodingPenguin <coodingpenguin@gmail.com>
2023-06-08 01:00:44 +0900

    Fix narrow blogTagList width

--- src/theme/BlogLayout/index.js
+++ src/theme/BlogLayout/index.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import BlogSidebar from '@theme/BlogSidebar';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import BlogSidebar from "@theme/BlogSidebar";
 
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
 
 export default function BlogLayout(props) {
-  const {sidebar, toc, children, ...layoutProps} = props;
+  const { sidebar, toc, children, ...layoutProps } = props;
   const hasSidebar = sidebar && sidebar.items.length > 0;
   return (
     <Layout {...layoutProps}>
-      <div className={clsx(styles.blogLayout, 'margin-vert--lg')}>
+      <div className={clsx(styles.blogLayout, "margin-vert--lg")}>
         <div className="row">
-          <BlogSidebar sidebar={sidebar}/>
+          <BlogSidebar sidebar={sidebar} />
           <main
-            className={clsx('col', {
-              'col--6': hasSidebar,
-              'col--9 col--offset-1': !hasSidebar,
+            className={clsx("col", {
+              "col--6": hasSidebar,
+              "col--9 col--offset-1": !hasSidebar,
             })}
             itemScope
-            itemType="http://schema.org/Blog">
+            itemType="http://schema.org/Blog"
+          >
             {children}
           </main>
-          {toc && <div className={clsx(styles.blogToc, 'col', 'col--3')}>{toc}</div>}
+          {toc && (
+            <div className={clsx(styles.blogToc, "col", "col--3")}>{toc}</div>
+          )}
         </div>
       </div>
     </Layout>
