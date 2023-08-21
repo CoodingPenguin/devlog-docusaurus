b80c09582176e3cd1f6874c84c86e44c9d70ca1a
CoodingPenguin <coodingpenguin@gmail.com>
2023-08-22 01:12:43 +0900

    Update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,34 @@
-import React from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
+import React from "react";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import { Content } from "@theme/BlogPostPage";
 
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 function HomepageProfile() {
   return (
     <header className={styles.profile}>
       <div className="container">
         <h1 className={styles.profile__title}>코딩하는펭귄</h1>
-        <p className={styles.profile__description}>☕️와 🌳를 좋아하는 개발자</p>
+
         <div className={styles.profile__image}>
-          <img src="https://avatars.githubusercontent.com/u/37505775?v=4" alt="GitHub Profile Image"/>
+          <img
+            src="https://avatars.githubusercontent.com/u/37505775?v=4"
+            alt="GitHub Profile Image"
+          />
         </div>
+        <p className={styles.profile__description}>
+          ☕️와 🌳를 좋아하는 개발자
+        </p>
       </div>
     </header>
   );
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.title}`}
-      description={`${siteConfig.tagline}`}
-    >
+    <Layout title={`${siteConfig.title}`} description={`${siteConfig.tagline}`}>
       <HomepageProfile />
     </Layout>
   );
